d4af549bde4f349017d95f286053443ee00894c2
Marcel Göhn <marcelgoehn@googlemail.com>
2025-12-09 14:17:52 +0100

    feat: added email check view

--- kanban_app/api/urls.py
+++ kanban_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListCreateBoardView
+from .views import ListCreateBoardView, EmailCheckView
 
 urlpatterns = [
-    path("boards/", ListCreateBoardView.as_view(), name="board-list")
+    path("boards/", ListCreateBoardView.as_view(), name="board-list"),
+    path("email-check/", EmailCheckView.as_view(), name="email-check")
 ]
--- kanban_app/api/views.py
+++ kanban_app/api/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from kanban_app.models import Board
 from .serializers import BoardListSerializer
-from rest_framework.permissions import IsAuthenticated
 # from .permissions import IsOwnerOrMember
 
 
@@ -12,3 +17,14 @@ class ListCreateBoardView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class EmailCheckView(APIView):
+    def get(self, req):
+        user = get_object_or_404(User, email=req.user.email)
+        data = {
+            "id": user.pk,
+            "email": user.email,
+            "fullname": user.username
+        }
+        return Response(data, status=status.HTTP_200_OK)
